fa41803fe7a7288cb7991680f485ab9e2e7f2100
Morgana Rodrigues <morgana.dias.rodrigues@hp.com>
2023-06-09 15:47:18 -0300

    trends_tweets script

--- trends-scripts/trend_monitoring.py
+++ trends-scripts/trend_monitoring.py
@@ -11,7 +11,7 @@ intervalo = 1800 # segundos
 tempo_monitorar = 12 # horas
 tempo_monitorar = tempo_monitorar * 3600 # (neste caso, 30min)
 
-with open("../trends-data/__trends.txt", 'a') as f:
+with open("../trends-data/trends.txt", 'a') as f:
     for i in range(int(tempo_monitorar/intervalo)):
         print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
         f.write(f"Date: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')} \n")
---
+++ trends-scripts/trend_tweets.py
@@ -0,0 +1,37 @@
+import json
+import re
+import snscrape.modules.twitter as sntwitter
+
+path = '../trends-data/trends.txt'
+trends = []
+
+with open(path) as f:
+    for line in f.readlines():
+        trends.append(str(line).rstrip())
+
+for index, item in enumerate(trends):
+    if str(item).startswith("D"): # excluindo as datas para termos só URLs e trend name
+        trends.remove(trends[index])
+
+trends_existentes = []
+trend_text = {}
+limit = 15
+
+for item in trends:
+    trend_name = re.search("Trend name: (.*)", str(item)).group().replace("Trend name: ", '').rstrip()
+    tweets = []
+    print("Nome da Trend: ", trend_name)
+    if trend_name not in trends_existentes:
+        print("Adicionando trend...")
+        trends_existentes.append(trend_name)
+        for tweet in sntwitter.TwitterSearchScraper(f'{trend_name} lang:en').get_items():
+            if len(tweets) == limit:
+                print("15 tweets adicionados.")
+                break
+            else:
+                tweets.append([tweet.rawContent])
+                trend_text[trend_name] = tweets
+
+
+with open('../trends-data/processed_trends.json', 'a') as file:
+     file.write(json.dumps(trend_text))
