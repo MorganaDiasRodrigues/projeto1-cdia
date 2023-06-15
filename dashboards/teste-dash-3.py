db3e5fa3ecb13c71dbbca30cf999657e318e4136
Morgana Rodrigues <morgana.dias.rodrigues@hp.com>
2023-06-15 08:57:50 -0300

    dash

--- dashboards/teste-dash-3.py
+++ dashboards/teste-dash-3.py
@@ -14,7 +14,7 @@ import plotly.graph_objects as go
 with open('../trends-data/date_time_trends.json', 'r') as f:
     date_time_trends = json.load(f)
 
-with open('../trends-scripts/trends_images.json', 'r') as f:
+with open('../trends-data/trends_images.json', 'r') as f:
     images_dict = json.load(f)
 
 available_dates = list(date_time_trends.keys())
@@ -23,7 +23,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_
 
 def encode_image(image_file):
     encoded = base64.b64encode(open(image_file, 'rb').read())
-    return f'data:image/jpeg;base64,{encoded.decode()}'
+    return f'data:image/png;base64,{encoded.decode()}'
 
 # Define the app layout
 app.layout = html.Div([
@@ -32,7 +32,7 @@ app.layout = html.Div([
     ], id='date-buttons'),
     html.Div(id='time-buttons'),
     html.Div(id='graph'),
-    html.Div(id='trend-detail', children=html.Div(style={'height': '400px'}))  # added empty space
+    html.Div("Select a trend name to see its color", id='trend-detail')
 ])
 
 @app.callback(
@@ -83,7 +83,7 @@ def update_figure(n_clicks, labels):
     State({'type': 'trend-link', 'index': ALL, 'date': ALL}, 'children'),
     prevent_initial_call=True
 )
-def display_trend_detail(n_clicks, labels):
+def update_trend_detail(n_clicks, labels):
     ctx = dash.callback_context
     if not ctx.triggered:
         return dash.no_update
@@ -91,35 +91,26 @@ def display_trend_detail(n_clicks, labels):
         button_id = ctx.triggered[0]['prop_id'].split('.')[0]
         button_info = json.loads(button_id)
         selected_trend = labels[button_info['index']]
+        selected_trend_stripped = selected_trend.strip("#")  # Remove "#" from the trend
 
-        image_data = images_dict[selected_trend]['images']
+        # Check if there are images for this trend
+        if selected_trend_stripped in images_dict:
+            image_elements = []
+            for image in images_dict[selected_trend_stripped]['images']:
+                image_elements.append(html.Img(src=encode_image(image['path']), height=200))
 
-        image_and_chart = []
-        for i, img in enumerate(image_data):
-            image_path = img['path']
-            dominant_colors = img['dominant_colors']
-            hex_codes = list(dominant_colors.keys())
-            color_proportions = list(dominant_colors.values())
-            encoded_image = encode_image(image_path)
-            image = html.Img(src=encoded_image, style={'width': '300px', 'height': '300px'})  # increased size
+                colors = image['dominant_colors']
+                color_names = list(colors.keys())
+                color_values = list(colors.values())
 
-            fig = go.Figure(
-                go.Pie(labels=hex_codes, values=color_proportions, text=hex_codes, 
-                       name=f"Image {i+1}", hoverinfo='label', hole=.4, 
-                       showlegend=False, pull=[0.2]*len(hex_codes), textinfo='label+percent')  
-                # added hex code to pie chart and show percentage of each color
-            )
+                pie_chart = dcc.Graph(id='color-chart', figure=go.Figure(
+                    data=[go.Pie(labels=color_names, values=color_values, hole=0.3)]))
 
-            fig.update_layout(
-                autosize=True,
-                margin=dict(t=10, b=10, l=10, r=10)
-            )
+                image_elements.append(pie_chart)
+        else:
+            image_elements = [html.P("There is no image saved for this trend")]
 
-            pie_chart = dcc.Graph(figure=fig, style={'width': '400px', 'height': '400px'})  # increased size
-
-            image_and_chart.append(html.Div([image, pie_chart], style={'display': 'inline-block', 'margin': '10px'}))
-
-        return html.Div(image_and_chart, style={'display': 'flex', 'justify-content': 'space-around', 'align-items': 'center'})
+        return image_elements
 
 
 if __name__ == '__main__':
